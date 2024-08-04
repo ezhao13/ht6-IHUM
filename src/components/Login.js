fb184a419674089e28945b0721d7ac0f44a04f6b
aryan-michael <aryanmichael24@gmail.com>
2024-08-04 10:03:48 -0400

    final push

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import './Login.css';
+import logo from './logo.png';
 
 const Login = () => {
   const { loginWithRedirect, isAuthenticated } = useAuth0();
@@ -14,10 +15,15 @@ const Login = () => {
   }, [isAuthenticated, navigate]);
 
   return (
-    <div className="login-container">
-      <button className="login-button" onClick={() => loginWithRedirect()}>
-        Log In
-      </button>
+    <div className="login-page">
+      <div className='home-logo'>
+        <img src={logo} alt="Logo" className="logo" />
+      </div>
+      <div className="login-container">
+        <button className="login-button" onClick={() => loginWithRedirect()}>
+          Log In
+        </button>
+      </div>
     </div>
   );
 };
